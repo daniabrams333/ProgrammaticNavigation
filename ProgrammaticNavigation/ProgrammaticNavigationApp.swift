6a379791027f9a71e0979a09e9e5245dcbebdb21
Danielle Hampton-Abrams <daniabrams333@gmail.com>
2025-05-23 18:44:08 -0400

    Initial Commit

---
+++ ProgrammaticNavigation/ProgrammaticNavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgrammaticNavigationApp.swift
+//  ProgrammaticNavigation
+//
+//  Created by Danielle Abrams on 5/23/25.
+//
+
+import SwiftUI
+
+@main
+struct ProgrammaticNavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
