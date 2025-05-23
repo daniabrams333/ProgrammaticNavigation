1baef83f83d617d8302b4be31f70e07aedc11329
Danielle Hampton-Abrams <daniabrams333@gmail.com>
2025-05-23 19:15:00 -0400

    inital commit

---
+++ ProgrammaticNavigation/Route.swift
@@ -0,0 +1,22 @@
+//
+//  Route.swift
+//  ProgrammaticNavigation
+//
+//  Created by Danielle Abrams on 5/23/25.
+//
+
+/// definition of all possible navigation destinations. Each case represents a unique screen or view that users can navigate to.
+///
+
+import Foundation
+
+enum Route: Hashable {
+    case home
+    case detail(id: Int)
+    case profile(username: String)
+    case settings
+    case categories(categoryId: Int)
+    case productList(categoryId: Int)
+    case productDetail(productId: Int)
+    
+}
---
+++ ProgrammaticNavigation/Router.swift
@@ -0,0 +1,61 @@
+//
+//  Router.swift
+//  ProgrammaticNavigation
+//
+//  Created by Danielle Abrams on 5/23/25.
+//
+
+import Foundation
+import SwiftUI
+
+class Router: ObservableObject {
+    @Published var path = NavigationPath()
+    
+    // ...methods
+    
+    /// function that adds a new destination to the navigation stack
+    /// pushes a new view onto the navigation hierarchy
+    /// handles forward navigation
+    
+    func navigate(to route: Route) {
+        path.append(route)
+    }
+    
+    /// removes the current view from the navigation stack
+    /// returns to the previous screen
+    /// handles backward navigation
+    
+    func navigateBack() {
+        path.removeLast()
+    }
+    
+    /// function to clear  the entire navigation stack.
+    /// returns to the root / home view
+    /// handles complete navigaiton reset.
+    
+    func navigateToRoot() {
+        path.removeLast(path.count)
+    }
+    
+    /// A function removes a specific number of views from the stack
+    /// enables custom back navigation
+    /// handles partial stack clearing
+    
+    func popToView(count: Int) {
+        path.removeLast(count)
+    }
+    
+    
+}
+
+/// Checks if backward navigation is possible
+///  Prevents navigation errors.
+/// Enables safe navigation handling.
+
+
+extension Router {
+    func canNavigateBack() -> Bool {
+        return path.count > 0
+    }
+}
+
